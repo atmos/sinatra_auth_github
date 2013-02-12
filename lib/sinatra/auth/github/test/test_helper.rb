b89beb165f17a331f3454fcbebcf99c8e29a242a
David Calavera <calavera@github.com>
2013-02-12 10:39:39 -0800

    Fix warden-github's user module name.

--- lib/sinatra/auth/github/test/test_helper.rb
+++ lib/sinatra/auth/github/test/test_helper.rb
@@ -11,7 +11,7 @@ module Sinatra
             User.make(attrs)
           end
 
-          class User < Warden::Github::Oauth::User
+          class User < Warden::Github::User
             def self.make(attrs = {})
               default_attrs = {
                  'login'   => "test_user",
